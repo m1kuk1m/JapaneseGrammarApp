d1a86673b44a894c00196ed39e17ff380687a7ba
m1kuk1m <sometimesnaive1@gmail.com>
2026-06-15 20:49:35 +0800

    chore: bump version to v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.japanesegrammarapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
