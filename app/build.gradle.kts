7caec25217f50868b063ddae862e7de3b3e4e294
m1kuk1m <sometimesnaive1@gmail.com>
2026-06-16 22:13:32 +0800

    chore: bump versionCode to 4 and versionName to 1.1.2 on main branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.japanesegrammarapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
