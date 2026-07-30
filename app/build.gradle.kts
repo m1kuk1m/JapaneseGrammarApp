d9a283dc72c084975b814e8542729a509ceea39f
m1kuk1m <sometimesnaive1@gmail.com>
2026-07-30 17:59:42 +0800

    chore(release): bump version to 1.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.example.japanesegrammarapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.9.1"
+        versionCode = 31
+        versionName = "1.9.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
